2eb0d335f503faa7e7fd68fe6e29ec02a3ee1cad
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-12-29 15:21:54 +0100

    MODIFY apollo client init for subscriptions

--- app/src/lib/apollo-client.ts
+++ app/src/lib/apollo-client.ts
@@ -1,6 +1,31 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloClient, InMemoryCache, split, HttpLink } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { getMainDefinition } from "@apollo/client/utilities";
+
+const httpLink = new HttpLink({
+  uri: `http://${process.env.NEXT_PUBLIC_API_URL}`
+});
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: `ws://${process.env.NEXT_PUBLIC_API_URL}`
+  })
+);
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 export const client = new ApolloClient({
-  uri: process.env.NEXT_PUBLIC_API_URL,
+  link: splitLink,
   cache: new InMemoryCache()
 });
