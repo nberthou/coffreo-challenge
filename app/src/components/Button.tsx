5127ff8fda83fb90e96ce83f8488ddbda6e825d6
Nicolas Berthou <berthou.nicolas@laposte.net>
2024-12-29 10:03:04 +0100

    ADD styling to button

--- app/src/components/Button.tsx
+++ app/src/components/Button.tsx
@@ -3,8 +3,27 @@ import { FC } from "react";
 type Button = {
   children: string;
   onClick: () => void;
+  variant: keyof typeof variants;
 };
 
-export const Button: FC<Button> = ({ children, onClick }) => {
-  return <button onClick={onClick}>{children}</button>;
+const variants = {
+  primary: {
+    backgroundColor: "#ffc107",
+    textColor: "#182030"
+  }
+};
+
+export const Button: FC<Button> = ({
+  children,
+  onClick,
+  variant = "primary"
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`bg-[${variants[variant].backgroundColor}] text-[${variants[variant].textColor}]`}
+    >
+      {children}
+    </button>
+  );
 };
